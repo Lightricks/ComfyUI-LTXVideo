1ae0aa67c9b2a22812ad23145e84d97268bc970c
Onza <ejgonzalezviera@gmail.com>
2025-08-17 01:01:34 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ from .nodes_registry import NODE_CLASS_MAPPINGS as RUNTIME_NODE_CLASS_MAPPINGS
 from .nodes_registry import (
     NODE_DISPLAY_NAME_MAPPINGS as RUNTIME_NODE_DISPLAY_NAME_MAPPINGS,
 )
-from .chunks_upscale_sampler import LTXVChunksUpscaleSampler, LTXVChunksUpscaleMerger
+from .chunks_upscale_sampler import LTXVChunksUpscaleSampler
 from .nodes_registry import NODES_DISPLAY_NAME_PREFIX, camel_case_to_spaces
 from .prompt_enhancer_nodes import LTXVPromptEnhancer, LTXVPromptEnhancerLoader
 from .q8_nodes import LTXVQ8LoraModelLoader, LTXVQ8Patch
@@ -62,7 +62,6 @@ NODE_CLASS_MAPPINGS = {
     "LTXVMultiPromptProvider": MultiPromptProvider,
     "ImageToCPU": ImageToCPU,
     "LTXVChunksUpscaleSampler": LTXVChunksUpscaleSampler,
-    "LTXVChunksUpscaleMerger": LTXVChunksUpscaleMerger,
 }
 
 # Consistent display names between static and dynamic node mappings in nodes_registry.py,
